e9f5b43656bf3b3e9175067e881ea81551753b61
Dede Mahendra <dededemahendra@gmail.com>
2024-06-09 14:27:09 +0800

    build: add auth

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -3,10 +3,14 @@ import MaxWithWrapper from '../common/MaxWithWrapper';
 import Link from 'next/link';
 import { buttonVariants } from '../ui/button';
 import { ArrowRight } from 'lucide-react';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 
-const Navbar = () => {
-  const user = undefined;
-  const isAdmin = false;
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  const isAdmin =
+    user?.email === process.env.ADMIN_EMAIL;
 
   return (
     <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
